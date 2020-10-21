5eae2e21ea5d0b8f4eaa16d1396f2c65620a3a52
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-10-21 20:53:48 +0530

    NSFuncButto

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [sol, setSol] = React.useState("");
+  const handleclick =() =>{
+    let line = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    setSol(line);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick = {handleclick}></button>
     </div>
   );
 }
